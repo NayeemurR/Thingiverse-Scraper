1a124c7e47adeb462abdc57767cfb3c19e37cb31
Nayeemur <nayeem3156@gmail.com>
2022-12-27 11:32:30 -0500

    tried our best to get the files of popular models :(

--- automate.py
+++ automate.py
@@ -4,10 +4,31 @@ import json
 url = "https://api.thingiverse.com/search/"
 querystring = {"sort": "popular"}
 headers = {
-    "Authorization": "Bearer **********"
+    "Authorization": "Bearer fe9995c480597be8a0b32a50ec9423c3"
     }
-
 
 response = requests.request("GET", url, headers=headers, params=querystring)
 response = json.loads(response.text)
-print(response)
+# print(response)
+data = response['hits']
+item_ids = {}
+
+for i in data:
+    item_ids[i['name']] = i['id']
+    
+# print(item_ids)
+
+for name in item_ids:
+    print(type(item_ids[name]))
+    url = f"https://api.thingiverse.com/things/files/{item_ids[name]}"
+    querystring = {}
+    headers = {
+        "Authorization": "Bearer fe9995c480597be8a0b32a50ec9423c3"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    response = json.loads(response.text)
+    print(response)
+
+# /files/{$id}/
+# url = "https://api.thingiverse.com/files/763622"
+
