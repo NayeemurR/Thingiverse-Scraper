2bf1aeaab1efd4be9a6a0ed671b38804af47fbb2
Nayeemur <nayeem3156@gmail.com>
2022-12-28 10:55:19 -0500

    working file capability

--- automate.py
+++ automate.py
@@ -4,7 +4,7 @@ import json
 url = "https://api.thingiverse.com/search/"
 querystring = {"sort": "popular"}
 headers = {
-    "Authorization": "Bearer *****"
+    "Authorization": "Bearer fe9995c480597be8a0b32a50ec9423c3"
     }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
@@ -21,23 +21,25 @@ def get_urls():
     return data_list
 
 
-# item_ids = {}
+item_ids = {}
 
-# for i in data:
-#     item_ids[i['name']] = i['id']
+for i in data:
+    item_ids[i['name']] = i['id']
     
-# # print(item_ids)
+# print(item_ids)
 
-# for name in item_ids:
-#     print(type(item_ids[name]))
-#     url = f"https://api.thingiverse.com/things/files/{item_ids[name]}"
-#     querystring = {}
-#     headers = {
-#         "Authorization": "Bearer ******"
-#     }
-#     response = requests.request("GET", url, headers=headers, params=querystring)
-#     response = json.loads(response.text)
-#     print(response)
+for name in item_ids:
+    # print(item_ids[name])
+    url = f"https://api.thingiverse.com/files/{item_ids[name]}" #TODO: This url takes in the 'id of a file,' not id of the item
+    # print(url)
+    querystring = {}
+    headers = {
+        "Authorization": "Bearer fe9995c480597be8a0b32a50ec9423c3"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    response = json.loads(response.text)
+    print(response)
+    print("#"*100)
 
 # /files/{$id}/
 # url = "https://api.thingiverse.com/files/763622"
